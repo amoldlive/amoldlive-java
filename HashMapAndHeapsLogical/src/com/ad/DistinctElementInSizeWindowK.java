8c85fedfabe3dae30b81ae9f5ac1cfb57d5c2bf4
amoldlive <32627412+amoldlive@users.noreply.github.com>
2021-06-26 09:00:29 +0530

    hashmap and heaps

---
+++ HashMapAndHeapsLogical/src/com/ad/DistinctElementInSizeWindowK.java
@@ -0,0 +1,18 @@
+package com.ad;
+
+import java.util.HashMap;
+
+import javax.print.DocFlavor.INPUT_STREAM;
+
+public class DistinctElementInSizeWindowK {
+	
+	public static final String str=new String("AS");
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+
+	}
+}
