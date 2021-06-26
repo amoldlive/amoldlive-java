68b90602c9ae8be7c0b98b71036fdbfbb5946416
amoldlive <32627412+amoldlive@users.noreply.github.com>
2021-06-26 09:18:32 +0530

    Ds  additional changes
    Ds  additional changes

--- Data Structure/src/LinkedListMain.java
+++ Data Structure/src/LinkedListMain.java
@@ -12,6 +12,51 @@ class Node {
 	}
 }
 
+class SinglyLinkedList {
+	Node head;
+
+	public void add(int data) {
+		Node node = new Node(data);
+		if (head == null) {
+			// Node node=new Node(data);
+			head = node;
+		} else {
+			Node temp = head;
+			while (temp.next != null) {
+				temp = temp.next;
+			}
+			
+			temp.next = node;
+		}
+	}
+	
+	public void print() {
+		Node temp = head;
+		while (temp.next != null) {
+			System.out.print(temp.data + " -> ");
+			temp = temp.next;
+		}
+		System.out.print(temp.data + " -> NULL ");
+		System.out.println();
+	}
+	
+	    public void reverse()
+	    {
+	    	Node current = head;
+		    Node prev = null;
+	        Node fwd = null;
+	       
+	        while (current != null) {
+	            fwd = current.next;
+	            current.next = prev;
+	            prev = current;
+	            current = fwd;
+	        }
+	        head = prev;
+	        //return node;
+	    }
+}
+
 class CustomLinkedList {
 
 	Node head;
@@ -30,41 +75,41 @@ class CustomLinkedList {
 			temp.next = node;
 		}
 	}
-	
+
 	public void remove(int data) {
 		Node temp = head;
-		
-		if(temp.data==data) {
-			head=temp.next;
+
+		if (temp.data == data) {
+			head = temp.next;
 			return;
 		}
-		
-		while(temp.next!=null) {
-			if(temp.next.data==data) {
-				temp.next=temp.next.next;
+
+		while (temp.next != null) {
+			if (temp.next.data == data) {
+				temp.next = temp.next.next;
 			}
-			if(temp.next!=null)
-			temp=temp.next;
+			if (temp.next != null)
+				temp = temp.next;
 		}
 	}
 
-	
 	public void removeFromEnd() {
 		Node temp = head;
-		
-		if(temp.next==null) {
-			head=null;
+
+		if (temp.next == null) {
+			head = null;
 			return;
 		}
-		
-		while(temp.next!=null) {
-			if(temp.next.next==null) {
-				temp.next=null;
+
+		while (temp.next != null) {
+			if (temp.next.next == null) {
+				temp.next = null;
 			}
-			if(temp.next!=null)
-			temp=temp.next;
+			if (temp.next != null)
+				temp = temp.next;
 		}
 	}
+
 	public void printList() {
 
 		Node temp = head;
@@ -74,141 +119,140 @@ class CustomLinkedList {
 		}
 		System.out.print(temp.data + " \n");
 	}
-	
+
 	public int findMin() {
-		
-		Node temp=head;
-		int min=0;
-		
-		if(temp.next==null) {
-			min=temp.data;
+
+		Node temp = head;
+		int min = 0;
+
+		if (temp.next == null) {
+			min = temp.data;
 			return min;
-		}else {
-			min=temp.data;
+		} else {
+			min = temp.data;
 		}
-		
-		while(temp.next!=null) {
-			
-			if(min>temp.data) {
-				min=temp.data;
+
+		while (temp.next != null) {
+
+			if (min > temp.data) {
+				min = temp.data;
 			}
-			
-			temp=temp.next;
+
+			temp = temp.next;
 		}
-		
+
 		return min;
 	}
-	
+
 	public int findMax() {
-		
-		Node temp=head;
-		int max=0;
-		
-		if(temp.next==null) {
-			max=temp.data;
+
+		Node temp = head;
+		int max = 0;
+
+		if (temp.next == null) {
+			max = temp.data;
 			return max;
-		}else {
-			max=temp.data;
+		} else {
+			max = temp.data;
 		}
-		
-		while(temp.next!=null) {
-			
-			if(max<temp.data) {
-				max=temp.data;
+
+		while (temp.next != null) {
+
+			if (max < temp.data) {
+				max = temp.data;
 			}
-			
-			temp=temp.next;
+
+			temp = temp.next;
 		}
-		
+
 		return max;
 	}
-	
+
 	public void printReverse() {
-		Node temp=head;
+		Node temp = head;
 		printReverse(temp);
 	}
+
 	private void printReverse(Node node) {
 
-		if(node.next==null) {
-			System.out.print(node.data+" ");
-			return ;
+		if (node.next == null) {
+			System.out.print(node.data + " ");
+			return;
 		}
-		
+
 		printReverse(node.next);
-		System.out.print(node.data+" ");
-		
-		
+		System.out.print(node.data + " ");
+
 	}
 
 	public void swapFirstNodeWithLastNode() {
-		Node tempHead=head;
-		if(tempHead.next==null) {
+		Node tempHead = head;
+		if (tempHead.next == null) {
 			return;
 		}
-		
-		Node temp=head;
-		
-		while(temp.next.next!=null) {
-			System.out.print(temp.next.data+" ");
-			temp=temp.next;
+
+		Node temp = head;
+
+		while (temp.next.next != null) {
+			System.out.print(temp.next.data + " ");
+			temp = temp.next;
 		}
-		
-		if(temp.next.next==null) {
-			head=temp.next;
-			head.next=tempHead.next;
-			temp.next=tempHead;
-			tempHead.next=null;
+
+		if (temp.next.next == null) {
+			head = temp.next;
+			head.next = tempHead.next;
+			temp.next = tempHead;
+			tempHead.next = null;
 		}
-		
+
 	}
-	
+
 	Node pleft;
+
 	public boolean isPallindrom() {
-		Node temp=head;
-		pleft=head;
+		Node temp = head;
+		pleft = head;
 		return isPallindromHelper(temp);
 	}
-	
+
 	public boolean isPallindromHelper(Node right) {
-		if(right==null) {
+		if (right == null) {
 			return true;
 		}
-		
-		boolean result=isPallindromHelper(right.next);
-		if(result==false) {
+
+		boolean result = isPallindromHelper(right.next);
+		if (result == false) {
 			return false;
-		}else if(pleft.data!=right.data) {
+		} else if (pleft.data != right.data) {
 			return false;
-		}else {
-			pleft=pleft.next;
+		} else {
+			pleft = pleft.next;
 			return true;
 		}
-		
-		
+
 	}
-	
+
 	public boolean isPallindromUsingStack() {
-		
-		Node temp=head;
-		Stack<Integer> st=new Stack<>();
-		
-		while(temp!=null) {
+
+		Node temp = head;
+		Stack<Integer> st = new Stack<>();
+
+		while (temp != null) {
 			st.push(temp.data);
-			temp=temp.next;
+			temp = temp.next;
 		}
-		
-		temp=head;
-		while(temp!=null) {
-			if(temp.data!=st.pop()) {
+
+		temp = head;
+		while (temp != null) {
+			if (temp.data != st.pop()) {
 				return false;
 			}
-			temp=temp.next;
+			temp = temp.next;
 		}
-		
+
 		return true;
 	}
-	
-	
+
 	/*
 	 * public void printUsingRecursion() { Node temp=head; printReverse(temp); }
 	 * private void printUsingRecursion(Node node) {
@@ -220,13 +264,24 @@ class CustomLinkedList {
 	 * 
 	 * }
 	 */
-	
+
 }
 
 public class LinkedListMain {
 
 	public static void main(String[] args) {
-
+		
+		SinglyLinkedList list=new SinglyLinkedList();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+	
+		list.print();
+		list.reverse();
+		list.print();
+	}
+	private static void linkedListTEst() {
 		CustomLinkedList list = new CustomLinkedList();
 		list.add(10);
 		list.add(20);
@@ -240,39 +295,39 @@ public class LinkedListMain {
 		list.add(70);
 		list.add(60);
 		list.printList();
-		System.out.println("min Node data : "+list.findMin());
-		System.out.println("max Node data : "+list.findMax());
-		
+		System.out.println("min Node data : " + list.findMin());
+		System.out.println("max Node data : " + list.findMax());
+
 		System.out.println("in reverse order : ");
 		list.printReverse();
-		
+
 		System.out.println("\nBefore swapping");
 		list.printList();
-		
+
 		System.out.println("interchanging nodes");
 		list.swapFirstNodeWithLastNode();
-		
+
 		System.out.println("\nAfter Sapping");
 		list.printList();
-		
+
 		CustomLinkedList list2 = new CustomLinkedList();
 		list2.add(10);
 		list2.add(20);
 		list2.add(30);
 		list2.add(20);
 		list2.add(10);
-		System.out.println("is Pallindrom : "+list2.isPallindrom());
-		System.out.println("is Pallindrom using stack: "+list2.isPallindromUsingStack());
-		
+		System.out.println("is Pallindrom : " + list2.isPallindrom());
+		System.out.println("is Pallindrom using stack: " + list2.isPallindromUsingStack());
+
 		CustomLinkedList list3 = new CustomLinkedList();
 		list3.add(10);
 		list3.add(20);
 		list3.add(30);
 		list3.add(40);
 		list3.add(50);
-		System.out.println("is Pallindrom : "+list3.isPallindrom());
-		System.out.println("is Pallindrom using stack: "+list3.isPallindromUsingStack());
-		
+		System.out.println("is Pallindrom : " + list3.isPallindrom());
+		System.out.println("is Pallindrom using stack: " + list3.isPallindromUsingStack());
+
 	}
 
 }
