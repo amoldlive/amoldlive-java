02a20f5ea6e308a7a0ba3df6f3ccf22247c54bc3
amoldlive <32627412+amoldlive@users.noreply.github.com>
2021-06-26 16:53:02 +0530

    package level changes
    package level changes

---
+++ LogicalPrograms/src/com/hashmap/and/heap/DistinctElementInSizeWindowK.java
@@ -0,0 +1,18 @@
+package com.hashmap.and.heap;
+
+import java.util.HashMap;
+
+import javax.print.DocFlavor.INPUT_STREAM;
+
+public class DistinctElementInSizeWindowK {
+	
+	public static final String str=new String("AS");
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+
+	}
+}
---
+++ LogicalPrograms/src/com/hashmap/and/heap/FindManagerAndHisEmployees.java
@@ -0,0 +1,63 @@
+package com.hashmap.and.heap;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class FindManagerAndHisEmployees {
+
+	public static void main(String[] args) {
+		/* Input */
+		/*
+		 * AC BC CF DE EF FF
+		 */
+		String[] str = { "AC", "BC", "CF", "DE", "EF", "FF" };
+
+		findManagerAndHisEMployee(str);
+
+	}
+
+	public static void findManagerAndHisEMployee(String[] str) {
+
+		HashMap<String, String> map = prepareMap(str);
+		System.out.println(map);
+		HashMap<String, HashSet<String>> outMap = new HashMap<String, HashSet<String>>();
+		String ceo;
+		for (String emp : map.keySet()) {
+
+			String manager = map.get(emp);
+
+			/* CEO manager==employee */
+			if (manager.equals(emp)) {
+				ceo = manager;
+			} else {
+
+				if (outMap.containsKey(manager)) {
+					HashSet<String> empSet=outMap.get(manager);
+					empSet.add(emp);
+					outMap.put(manager, empSet);
+					
+				} else {
+
+					HashSet<String> empSet = new HashSet<>();
+					empSet.add(emp);
+					outMap.put(manager, empSet);
+				}
+			}
+
+		}
+
+		System.out.println(outMap);
+	}
+
+	private static HashMap<String, String> prepareMap(String[] str) {
+
+		HashMap<String, String> map = new HashMap<String, String>();
+
+		for (String empManPair : str) {
+			char[] ch = empManPair.toCharArray();
+
+			map.put(ch[0] + "", ch[1] + "");
+		}
+		return map;
+	}
+}
