e9a72627394765298ee3c8c52f9675dc51fcfffa
amoldlive <32627412+amoldlive@users.noreply.github.com>
2021-07-03 10:41:13 +0530

    update
    update

--- LogicalPrograms/src/com/string/UpperLowerConversion.java
+++ LogicalPrograms/src/com/string/UpperLowerConversion.java
@@ -4,7 +4,14 @@ public class UpperLowerConversion {
 
 	public static void main(String[] args) {
 		String str = "gRaB";
-		System.out.println(convert(str));
+		System.out.println(convert(str) );
+		
+		System.out.println();
+		
+		
+		str = "PEPCodinG";
+		System.out.println("Original >> "+str);
+		ConvertWithoutUsingInBuildFunction(str);
 	}
 
 	static String convert(String str) {
@@ -23,4 +30,24 @@ public class UpperLowerConversion {
 		return newStr;
 	}
 
+	private static void ConvertWithoutUsingInBuildFunction(String str) {
+		StringBuilder sb=new StringBuilder(str);
+		
+		for (int i = 0; i <sb.length(); i++) {
+				char ch=sb.charAt(i);
+				if(ch>='a' && ch<='z') {
+					//lower case character
+					char upperCaseCh=(char) (ch - 'a' + 'A');
+					sb.setCharAt(i, upperCaseCh);
+					
+				}else if (ch>='A' && ch<='Z') {
+					//upper case character
+					char lowerCaseCh= (char) (ch -'A' + 'a');
+					sb.setCharAt(i, lowerCaseCh);
+				}
+				
+		}
+		System.out.println("Modified >> "+sb.toString());
+	}
+	
 }
