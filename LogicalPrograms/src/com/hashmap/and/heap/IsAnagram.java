1c5c9e65f5ca60af919ebe1dbae00092d9332320
amoldlive <32627412+amoldlive@users.noreply.github.com>
2021-07-21 22:12:54 +0530

    code changes
    code changes

--- LogicalPrograms/src/com/hashmap/and/heap/IsAnagram.java
+++ LogicalPrograms/src/com/hashmap/and/heap/IsAnagram.java
@@ -2,34 +2,49 @@ package com.hashmap.and.heap;
 
 import java.util.HashMap;
 
+import com.arrays.ArrayUtility;
+
 public class IsAnagram {
 	public static void main(String[] args) {
 
 		System.out.println(isAnagram("abbaccs", "sccaabb"));
 		System.out.println(isAnagram("abbaccs", "sccaabd"));
+		System.out.println(isAnagram("a", "ab"));
 
+		System.out.println(isAnagramWithoutHash("abbaccs", "sccaabb"));
 	}
-
+	private static boolean isAnagramWithoutHash(String s1, String s2) {
+		
+		  int[] aa=new int[26];
+	        if(s1.length()!=s2.length())
+	            return false;
+	        
+	        for(char a:s1.toCharArray())
+	               aa[a-'a']++;
+	        
+	        for(char a:s2.toCharArray())
+				   aa[a-'a']--;
+	        
+	        for(int i=0;i<26;i++)
+	            if(aa[i]!=0)
+	                return false;
+	        
+	       
+	        return true;
+	}
 	private static boolean isAnagram(String s1, String s2) {
 		boolean ans = false;
 		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		HashMap<Character, Integer> map2 = new HashMap<Character, Integer>();
 
 		for (int i = 0; i < s1.length(); i++) {
 			map.put(s1.charAt(i), map.getOrDefault(s1.charAt(i), 0) + 1);
 		}
-
+		
 		for (int i = 0; i < s2.length(); i++) {
-			if (map.containsKey(s2.charAt(i))) {
-
-				if (map.get(s2.charAt(i)) == 1) {
-					map.remove(s2.charAt(i));
-				} else {
-					map.put(s2.charAt(i), map.get(s2.charAt(i)) - 1);
-				}
-
-			}
+			map2.put(s2.charAt(i), map2.getOrDefault(s2.charAt(i), 0) + 1);
 		}
 
-		return map.size() == 0 ? true : false;
+		return map.equals(map2);
 	}
 }
