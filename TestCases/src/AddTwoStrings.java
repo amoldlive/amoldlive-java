36d65146006ac8861f42a0d310dafdd3e0d111fb
amoldlive <32627412+amoldlive@users.noreply.github.com>
2021-07-23 07:25:16 +0530

    notes changes
    notes changes

---
+++ TestCases/src/AddTwoStrings.java
@@ -0,0 +1,16 @@
+
+public class AddTwoStrings {
+	public static void main(String[] args) {
+		System.out.println(addStrings("11", "123"));
+	}
+
+	public static String addStrings(String num1, String num2) {
+		String ans="";
+			System.out.println("inside Method");
+			
+			
+			
+			
+		return ans;
+	}
+}
